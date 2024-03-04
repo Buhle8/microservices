7125dbbf5fa4b3a860c2155c917033a025ed905e
nonto <nontokozo@protogen.co.za>
2024-03-04 15:36:50 +0200

    migrating from the H2 database to postgresql database

--- services/reservations-service/src/main/java/za/co/protogen/domain/feign/UserFeign.java
+++ services/reservations-service/src/main/java/za/co/protogen/domain/feign/UserFeign.java
@@ -4,8 +4,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import za.co.protogen.domain.restTemplate.User;
 
 @FeignClient(name = "user-service")
